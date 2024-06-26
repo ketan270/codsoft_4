fc6501f5efa9e2a9e903dea300a4084accb5adb1
ketan270 <ketansharma2005@gmail.com>
2024-06-26 12:17:15 +0530

    C++ Practice Questions

---
+++ Lecture1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a= 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    unsigned int b = 122;
+    cout << b << endl;
+
+    float c = 2.0/5;
+    cout << c << endl;
+
+    int d = 97;
+    int e = 2;
+    bool first = (d==a);
+    cout << first << endl;
+} 
---
+++ Lecture2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a,b;
+    // cin>> a >> b;
+    // cout << "Value of a is " << a << endl << "Value of b is " << b << endl;
+    // cout <<"Value of a is : " << a << endl;
+    // if (a>0){
+    //     cout << "A is Positive" << endl;
+    // }
+    // else if (a==0){
+    //     cout << "A is Zero" << endl;
+    // }
+    // else {
+    //     cout << "A is Negative" << endl;
+    // }
+    // if (a>b){
+    //     cout << a << endl;
+    // }
+    // else {
+    //     cout << b << endl;
+    // }
+    // char ch;
+    // cout << "ENTER CHARACTER: " ;
+    // cin >> ch;
+    // if (ch>=97 && ch<=122){
+    //     cout << "CHARACTER IS LOWER CASE" << endl;
+    // }
+    // else if (ch>=65 && ch<=90){
+    //     cout << "CHARACTER IS UPPER CASE" << endl;
+    // }
+    // else if (ch>='0' && ch<='9'){
+    //     cout << "CHARACTER IS A NUMBER" << endl;
+    // }
+    // else {
+    //     cout << "ERROR" << endl;
+    // }
+    // int a;
+    // cin >> a;
+    // int i = 1;
+    // int sum = 0;
+    // while (i<=a)
+    // {
+    //     sum = sum + i;
+    //     i++;
+    //     cout << "Value of sum is : "  << sum << endl;
+    // }
+    
+}
